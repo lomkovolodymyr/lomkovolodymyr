74b27f78a5610e52962c1036a1dad54af8c3c394
lomkovolodymyr <lomkovolodymyr@gmail.com>
2021-12-07 15:09:36 +0200

    Update script.js

--- Coding Challenge JS/Coding Challenge #4/script.js
+++ Coding Challenge JS/Coding Challenge #4/script.js
@@ -1,5 +1,5 @@
 const money = prompt('Enter the number of the money and open console (F12 button)');
-let tip = money >= 50 && money <= 300 ? `${money * 0.15 }` : `${money * 0.20}`;
+let tip = money >= 50 && money <= 300 ? `${money * .15  }` : `${money * .20}`;
 console.log(tip);
 
-console.log(`Tips is ${tip}, value is ${money} , and the finale value is ${money - tip} `); 
+console.log(`Tips is ${tip}, value is ${money} , and the finale value is ${money*1 + tip*1} `); 
