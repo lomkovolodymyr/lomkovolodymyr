5c4c24c138bb2c1e1a24f9d3cce80fba70e5cfce
lomkovolodymyr <lomkovolodymyr@gmail.com>
2022-01-03 11:37:13 +0200

    timer JS

---
+++ timer JS/script.js
@@ -0,0 +1,31 @@
+//'use strict'
+let date = new Date('Jan 31 2022 17:42:31')
+
+function counts() {
+    let now = new Date();
+    gap = date - now;
+    console.log(gap);
+
+    let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
+    let minutes = Math.floor(gap / 1000 / 60) % 60;
+    let seconds = Math.floor(gap / 1000) % 60;
+    console.log(days);
+
+    if (gap < 0) {
+        document.getElementById('tGroup').innerText = "Promotion ended";
+    } else {
+        document.getElementById('d').innerText = days + ' Days';
+        document.getElementById('h').innerText = hours + ' Hours';
+        document.getElementById('m').innerText = minutes + ' Minutes';
+        document.getElementById('s').innerText = seconds + ' Seconds';
+    }
+
+    document.getElementById('d').innerText = days + ' Days';
+    document.getElementById('h').innerText = hours + ' Hours';
+    document.getElementById('m').innerText = minutes + ' Minutes';
+    document.getElementById('s').innerText = seconds + ' Seconds';
+}
+counts();
+
+setInterval(counts, 1000);
